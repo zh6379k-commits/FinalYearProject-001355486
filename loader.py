bd8cf77910c2b566d63c798b2dd8efd0a218f0a5
zh6379k-commits <zh6379k@gre.ac.uk>
2026-07-08 16:41:40 +0100

    Update loader.py
    Use the natsort library and import a function to sort the eeg files so they are sequential

--- loader.py
+++ loader.py
@@ -1,11 +1,13 @@
 
 import os
+from natsort import natsorted
 
 
 # Get Data
 eeg_data_path = "data/EEG"
 aud_data_path = "data/AUDIO"
 eeg_files = os.listdir(eeg_data_path)
+eeg_files = natsorted(eeg_files)
 audio_files = os.listdir(aud_data_path)
 
 
